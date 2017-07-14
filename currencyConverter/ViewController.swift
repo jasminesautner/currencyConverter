070317a43cba85d409c94ab47c7e035ab5c9f97d
Jasmine <jasmineivysautner@gmail.com>
2017-07-14 10:17:09 -0700

    add 'currencyConverter' project.

--- currencyConverter/ViewController.swift
+++ currencyConverter/ViewController.swift
@@ -9,7 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum ExchangeCurrency: Int {
+        case euroExchangeRate
+        case yenExhangeRate
+        case poundExchangeRate
+        case francExchangeRate
+        case canadianExchangeRate
+    }
+    
+    @IBOutlet weak var amountEntryField: UITextField!
+    @IBOutlet weak var currencySelector: UISegmentedControl!
+    @IBOutlet weak var convertedCurrencyField: UITextField!
+    
+    @IBAction func convertCurrencyButton(_ sender: UIButton) {
+ 
+        if let currencyAmount = Double(amountEntryField.text!) {
+        var exchangeRate = 0.0
+            /*
+            if let exchangeCurrency = ExchangeCurrency(rawValue: currencySelector.selectedSegmentIndex) {
+                switch exchangeCurrency {
+                case .euroExchangeRate:
+                }
+            }
+            */
+            
+            switch currencySelector.selectedSegmentIndex {
+            case 0: exchangeRate = 0.88
+            case 1: exchangeRate = 113.22
+            case 2: exchangeRate = 0.773991
+            case 3: exchangeRate = 0.97
+            case 4: exchangeRate = 1.30
+            default: break
+                
+            }
+        
+            let totalAmount = exchangeRate * currencyAmount
+            
+            if (!amountEntryField.isEditing) {
+                amountEntryField.text = String(format: "%.2f", amountEntryField)
+            }
+            //convertedCurrencyField.text = String(format: "%.2f", convertedCurrencyField)
+            // convertedCurrencyField.text = String(totalAmount) //String(totalAmount)
+            convertedCurrencyField.text = String(format: "%.2f", totalAmount)
+            
+        } else {
+            amountEntryField.text = ""
+            convertedCurrencyField.text = ""
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +67,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
